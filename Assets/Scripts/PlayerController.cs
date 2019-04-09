af1e851ce766bde571e38da4c577f90e9c1121d3
Dmytro Nasyrov <dmytro@pharosproduction.com>
2019-04-10 01:10:42 +0300

    add basic level and char

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public Animator anim;
+    public float movementSpeed;
+    float horizontalMovementSpeed;
+    bool isJumping;
+    bool isHurt;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController2D>();
+        isJumping = false;
+        isHurt = false;
+    }
+
+    void Update() {
+        horizontalMovementSpeed = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        anim.SetFloat("speed", Mathf.Abs(horizontalMovementSpeed));
+    }
+
+    private void FixedUpdate() {
+        controller.Move(horizontalMovementSpeed * Time.fixedDeltaTime, false, false);
+        isJumping = false;
+    }
+}
